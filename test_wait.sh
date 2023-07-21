ce98f14eb6b0bdb5986a73dcf702fce5b463ec00
thananauto <thananjayan1988@gmail.com>
2023-07-21 19:59:39 +0530

    Added chages

---
+++ test_wait.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#we have named the container ‘my-con’ in the above command so we are using that name in below #command, however, we can use contaienr ID but we need to get the id that required to write some #additional lines of code
+exit_code="$(docker wait gorest-api)"
+#Compare the status code
+while [ $exit_code -eq "0" ];
+do
+    echo “Job has encountered some error.”
+    echo container code :$exit_code
+done
+
